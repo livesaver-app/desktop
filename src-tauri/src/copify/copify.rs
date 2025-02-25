03313103581bb596502e9f94df50028639ee93ca
Magnus Enholm <magnuen2k@gmail.com>
2025-02-25 21:49:10 +0100

    handle rust io errors

--- src-tauri/src/copify/copify.rs
+++ src-tauri/src/copify/copify.rs
@@ -6,14 +6,20 @@ use std::io;
 use std::path::Path;
 use tauri::Emitter;
 
+use super::constants::*;
+
 #[tauri::command]
 pub async fn copify(window: tauri::Window, settings: CopifySettings) -> Result<(), String> {
     let files = find_by_extension(settings.folder.as_str(), "als");
 
+    if files.is_empty() {
+        return Err("No Ableton Live project files found".to_string());
+    }
+
     for (i, file_path) in files.iter().enumerate() {
         // Define the output file path by replacing the extension
         let mut xml = file_path.clone();
-        xml.set_extension("xml");
+        xml.set_extension(XML);
 
         if settings.create_backup {
             create_backup(file_path).map_err(|e| e.to_string())?;
@@ -32,9 +38,7 @@ pub async fn copify(window: tauri::Window, settings: CopifySettings) -> Result<(
 }
 
 fn create_backup(input: &Path) -> Result<(), io::Error> {
-    let app_backup_suffix = ".als.lsbak";
-
-    if (!input.ends_with(".als") || input.to_string_lossy().contains(app_backup_suffix)) {
+    if (!input.ends_with(ALS_EXTENSION) || input.to_string_lossy().contains(ALS_BACKUP_EXTENSION)) {
         println!("Input file is not valid to backup")
     }
 
@@ -45,17 +49,14 @@ fn create_backup(input: &Path) -> Result<(), io::Error> {
         .file_name()
         .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "Invalid file path"))?;
 
-    // Construct the backup filename with the suffix appended
     let backup_filename = format!(
         "{}{}",
-        filename.to_string_lossy().replace(".als", ""),
-        app_backup_suffix
+        filename.to_string_lossy().replace(ALS_EXTENSION, ""),
+        ALS_BACKUP_EXTENSION
     );
 
-    // Build the full path for the backup file
     let backup_file_ref = dir.join(backup_filename);
 
-    // Copy the file to the backup file path
     fs::copy(input, &backup_file_ref)?;
 
     Ok(())
