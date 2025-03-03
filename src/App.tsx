a2abeef31866fa2dab2ab58b7d0303e5e5de52b7
Magnus Enholm <magnuen2k@gmail.com>
2025-03-03 23:22:33 +0100

    test supabase for auth

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { ThemeProvider } from '@/components/theme-provider.tsx'
 import { DraggableTopBar } from '@/components/top-bar.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Copify } from './app/copify'
+import { useState, useEffect } from 'react'
+import { AuthSession } from '@supabase/supabase-js'
+import { supabase } from './supabaseClient'
+import Auth from './app/auth'
 
 const router = createBrowserRouter([
   {
@@ -16,10 +20,22 @@ const router = createBrowserRouter([
 ])
 
 function App() {
+  const [session, setSession] = useState<AuthSession | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  console.log(session)
   return (
     <ThemeProvider defaultTheme={'dark'} storageKey={'vite-ui-theme'}>
       <DraggableTopBar />
-      <RouterProvider router={router} />
+      {!session ? <Auth /> : <RouterProvider router={router} />}
     </ThemeProvider>
   )
 }
