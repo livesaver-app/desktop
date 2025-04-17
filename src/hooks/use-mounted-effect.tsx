ee91c42f7ba3a1637193235dcf3b63cf356f6bd6
Magnus Enholm <magnuen2k@gmail.com>
2025-04-17 14:32:48 +0200

    deactivate mounted effect (causing some wierd issues)

--- src/hooks/use-mounted-effect.tsx
+++ src/hooks/use-mounted-effect.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 
 export function useMountedEffect(effect: () => void | (() => void), deps: any[]) {
-  const hasMounted = useRef(false)
+  // const hasMounted = useRef(false)
 
   useEffect(() => {
-    if (!hasMounted.current) {
-      hasMounted.current = true
-      return
-    }
+    // if (!hasMounted.current) {
+    //   hasMounted.current = true
+    //   return
+    // }
 
     return effect()
   }, deps)
